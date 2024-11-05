ce1862d911ebda32961857f784514c9479f02970
Pedro Silva Melo Gomes Lima <221.14.030@EDU.gruposete.local>
2024-11-04 22:28:02 -0300

    atualização

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.lista_tarefas, name='lista_tarefas'),
+    path('', views.lista_tarefas, name='lista_tarefas'),  # Define '' para acessar diretamente a lista de tarefas
     path('adicionar/', views.adicionar_tarefa, name='adicionar_tarefa'),
+    path('concluir/<int:id>/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('excluir/<int:id>/', views.excluir_tarefa, name='excluir_tarefa'),
+    path('editar/<int:id>/', views.editar_tarefa, name='editar_tarefa'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,9 @@
 # tarefas/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
+from .models import Tarefa
+
+# tarefas/views.py
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
 
 def lista_tarefas(request):
@@ -10,5 +14,36 @@ def adicionar_tarefa(request):
     if request.method == "POST":
         titulo = request.POST.get('titulo')
         descricao = request.POST.get('descricao')
-        Tarefa.objects.create(titulo=titulo, descricao=descricao)
-    return redirect('lista_tarefas')  # Redireciona para a página de lista de tarefas
+        print(f"Título: {titulo}, Descrição: {descricao}")  # Log para verificação
+
+        if titulo:  # Adiciona verificação para garantir que o título não está vazio
+            Tarefa.objects.create(titulo=titulo, descricao=descricao)
+        else:
+            print("Título está vazio.")  # Log para verificar dados vazios
+
+    return redirect('lista_tarefas')
+
+
+
+def concluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.concluida = True
+    tarefa.save()
+    return redirect('lista_tarefas')
+
+def excluir_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+    tarefa.delete()
+    return redirect('lista_tarefas')
+
+def editar_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+
+    if request.method == 'POST':
+        novo_titulo = request.POST.get('titulo')
+        if novo_titulo:
+            tarefa.titulo = novo_titulo
+            tarefa.save()
+            return redirect('lista_tarefas')
+
+    return render(request, 'editar_tarefa.html', {'tarefa': tarefa})
